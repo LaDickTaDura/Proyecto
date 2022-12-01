9f5e0014658c29d7e587510f7f675f329e248111
LaDickTaDura <119633947+LaDickTaDura@users.noreply.github.com>
2022-12-01 17:59:01 -0500

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -7,7 +7,7 @@ st.write("El Licenciamiento Institucional es un procedimiento obligatorio para t
          
          
          
-n= st.slider("n", 5, 100, step=1)
+n= st.slider("holawas", 5, 100, step=1)
 chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
 st.line_chart(chart_data)
 
