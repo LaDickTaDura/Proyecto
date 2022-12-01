14484533af662a64d61cadd3844b4471ea775ebe
LaDickTaDura <119633947+LaDickTaDura@users.noreply.github.com>
2022-12-01 17:53:10 -0500

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+st.title("Licenciamiento Institucional")
+st.write("El Licenciamiento Institucional es un procedimiento obligatorio para todas las universidades del país
+         
+         
+         
 n = st.slider("n", 5, 100, step=1)
 chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
 st.line_chart(chart_data)
